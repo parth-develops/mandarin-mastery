01e825d472d7e180c20ec6a0d8f27468a5577ab9
Parth Umrania <pumrania9@gmail.com>
2024-06-13 00:05:55 +0530

    Added pinyin tooltip

--- app/dashboard/chapters/[slug]/page.jsx
+++ app/dashboard/chapters/[slug]/page.jsx
@@ -9,27 +9,85 @@ import {
     TableHeader,
     TableRow,
 } from "@/components/ui/table";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { TbBulb } from "react-icons/tb";
 
 export default async function ChapterPage({ params }) {
 
     const { slug } = params;
     const chapter = await fetchChapterBySlug(slug);
-    const json = JSON.stringify(chapter);
     const chapterContent = chapter.content.toObject();
-    console.log(chapterContent);
 
     return (
         <div className="flex flex-col flex-1">
             <h1 className="text-lg font-semibold md:text-2xl mb-4">{chapter.title}</h1>
+            <div className="flex items-center mb-2"><TbBulb size={24} className="mr-1" /> Hover the chinese text to see the pinyin</div>
             <div className="cards p-4 flex-1 flex gap-4 border rounded-lg shadow-sm">
-                <Table >
+
+                <Table className="mb-5">
                     <TableBody>
+                        <h2 className="font-bold mb-4">Bare Minimum</h2>
                         {
                             chapterContent.minimum.map(point => (
                                 <TableRow key={point._id.toString()}>
                                     <TableCell>{point.english}</TableCell>
-                                    <TableCell>{point.chinese}</TableCell>
-                                    <TableCell>{point.pinyin}</TableCell>
+                                    <TableCell>
+                                        <TooltipProvider>
+                                            <Tooltip>
+                                                <TooltipTrigger>
+                                                    {point.chinese}
+                                                </TooltipTrigger>
+                                                <TooltipContent side="right">
+                                                    <p>{point.pinyin}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+                        <h2 className="font-bold mt-5 mb-3">For brave ones</h2>
+                        {
+                            chapterContent.brave.map(point => (
+                                <TableRow key={point._id.toString()}>
+                                    <TableCell>{point.english}</TableCell>
+                                    <TableCell>
+                                        <TooltipProvider>
+                                            <Tooltip>
+                                                <TooltipTrigger>
+                                                    {point.chinese}
+                                                </TooltipTrigger>
+                                                <TooltipContent side="right">
+                                                    <p>{point.pinyin}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </TableCell>
+                                </TableRow>
+                            ))
+                        }
+                        <h2 className="font-bold mt-5 mb-3">For those who want more</h2>
+                        {
+                            chapterContent.more.map(point => (
+                                <TableRow key={point._id.toString()}>
+                                    <TableCell>{point.english}</TableCell>
+                                    <TableCell>
+                                        <TooltipProvider>
+                                            <Tooltip>
+                                                <TooltipTrigger>
+                                                    {point.chinese}
+                                                </TooltipTrigger>
+                                                <TooltipContent side="right">
+                                                    <p>{point.pinyin}</p>
+                                                </TooltipContent>
+                                            </Tooltip>
+                                        </TooltipProvider>
+                                    </TableCell>
                                 </TableRow>
                             ))
                         }
