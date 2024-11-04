c7371b3c259d3d713ef8f1953b8239a19c1f786c
Parth Umrania <pumrania9@gmail.com>
2024-11-04 23:46:06 +0530

    Dashboard responsiveness - added vertical scroll for dashboard content

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -17,7 +17,7 @@ export default async function Layout({ children }) {
             <GlobalProgressBar />
             <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
                 <div className="hidden border-r bg-muted/40 md:block">
-                    <div className="flex h-full max-h-screen flex-col gap-2 fixed">
+                    <div className="flex h-full max-h-screen flex-col gap-2">
                         <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                             <Link href="/dashboard" className="flex items-center gap-2 font-semibold">
                                 <span className="">Mandarin Mastery</span>
@@ -29,7 +29,7 @@ export default async function Layout({ children }) {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col">
+                <div className="flex flex-col overflow-auto">
                     <Header>
                         <UserInfo />
                     </Header>
